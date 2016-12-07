6e91fcc726f7a13996f3fbf860e9e17a60bdd60b
Andrii Romanenko <roand@ukr.net>
2016-12-07 16:25:19 +0200

    method RavCodebreaker.play changed

--- lib/rav_codebreaker.rb
+++ lib/rav_codebreaker.rb
@@ -1,21 +1,12 @@
 require_relative 'rav_codebreaker/version'
 require_relative 'rav_codebreaker/game'
 require_relative 'rav_codebreaker/console'
-
 module RavCodebreaker
-  def self._play(level)
-    begin
-      game = Game.new(level)
-      game.play
-    end while game.again?
-  end
-
   def self.play(level)
     begin
       console = Console.new(level)
       console.play
     end while console.again?
   end
-
 end
 
